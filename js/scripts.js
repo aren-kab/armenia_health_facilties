86338f7cd707d42ae82dd3d9d5f9c9d84d222a52
aren-kab <akabarajian@gmail.com>
2021-03-03 13:11:01 -0500

    Initialize_repo

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJlbi1rYWIiLCJhIjoiY2tsMTJhejk3MHhxazJxcW5sbGo0d3R3bSJ9.iuLNkGjRTZohqeqRzq-r7g';
+
+var map = new mapboxgl.Map({
+  container: 'mapContainer', // container ID
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
+  center: [45.12267497723875, 40.27928204045164], // starting position [lng, lat]
+  zoom: 7.5 // starting zoom
+});
